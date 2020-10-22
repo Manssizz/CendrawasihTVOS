d0fa748b009f8f825a5ec2621817a97a04462e4b
Manssizz <fajarmanssizz@gmail.com>
2020-10-22 20:39:10 +0700

    deleted:    kitchen/rootfs/app/Bluetooth/Bluetooth.apk 	deleted:    kitchen/rootfs/app/Bluetooth/lib/arm/libbluetooth_jni.so 	deleted:    kitchen/rootfs/app/Bluetooth/oat/arm/Bluetooth.odex 	deleted:    kitchen/rootfs/app/GilzCleaner.apk 	deleted:    kitchen/rootfs/app/GoogleTTS/GoogleTTS.apk 	deleted:    kitchen/rootfs/app_install/NF21_3.3.apk 	deleted:    kitchen/rootfs/app_install/netflix-6.26.1_build-15-31696.apk 	deleted:    kitchen/rootfs/priv-app/TvSettings/TvSettings.apk 	deleted:    kitchen/rootfs/priv-app/TvSettings/oat/arm/TvSettings.odex 	deleted:    kitchen/rootfs/priv-app/luki_8.9.3.apk 	modified:   oprek.sh 	kitchen/rootfs/app/klampok.apk 	kitchen/rootfs/app/luki_8.9.3.apk 	kitchen/rootfs/app_install/NF21_3.3.apk.bak 	kitchen/rootfs/app_install/NetflixTV.apk 	kitchen/rootfs/app_install/netflix-6.26.1_build-15-31696.apk.bak

--- oprek.sh
+++ oprek.sh
@@ -4,107 +4,107 @@
 SYSDIR="kitchen/system"
 SYSIMG="kitchen/system.img"
 
-cp -v original/system.img $SYSIMG || exit 1
-chmod 777 $SYSIMG
+sudo cp -v original/system.img $SYSIMG || exit 1
+sudo chmod 777 $SYSIMG
 
 
 echo "Mounting system.img to $SYSDIR"
 [ -d $SYSDIR ] || mkdir $SYSDIR
-mount -o loop,noatime,rw,sync $SYSIMG $SYSDIR
+sudo mount -o loop,noatime,rw,sync $SYSIMG $SYSDIR
 
 echo "Remove 3rd Party Apps (indihome)"
-rm -rf $SYSDIR/preinstall
+sudo rm -rf $SYSDIR/preinstall
 
 echo "Remove Unwanted Apps"
 # Must be removed, or you got Iptv Err
-rm -f $SYSDIR/app/MainControl.apk
-rm -f $SYSDIR/app/sqm.apk
-rm -rf $SYSDIR/app/apk
+sudo rm -f $SYSDIR/app/MainControl.apk
+sudo rm -f $SYSDIR/app/sqm.apk
+sudo rm -rf $SYSDIR/app/apk
 
-rm -f $SYSDIR/app/AppsMgr-release.apk
-rm -f $SYSDIR/app/ajvm.apk
-rm -f $SYSDIR/app/AptoideTV-3.3.1-useeapps.apk
-rm -f $SYSDIR/app/FactoryTestTool.apk
-rm -f $SYSDIR/app/iptvclient_boot-release.apk
-rm -f $SYSDIR/app/popup-release-signed.apk
-rm -f $SYSDIR/app/ZTEUpgrade.apk
-rm -f $SYSDIR/app/ZTEBrowser.apk
-rm -f $SYSDIR/app/ZTEPlayer.apk
+sudo rm -f $SYSDIR/app/AppsMgr-release.apk
+sudo rm -f $SYSDIR/app/ajvm.apk
+sudo rm -f $SYSDIR/app/AptoideTV-3.3.1-useeapps.apk
+sudo rm -f $SYSDIR/app/FactoryTestTool.apk
+sudo rm -f $SYSDIR/app/iptvclient_boot-release.apk
+sudo rm -f $SYSDIR/app/popup-release-signed.apk
+sudo rm -f $SYSDIR/app/ZTEUpgrade.apk
+sudo rm -f $SYSDIR/app/ZTEBrowser.apk
+sudo rm -f $SYSDIR/app/ZTEPlayer.apk
 
-rm -f $SYSDIR/app/VideoTestTool.apk
-rm -rf $SYSDIR/app/QuickSearchBox
-rm -rf $SYSDIR/app/NetworkTest
-rm -rf $SYSDIR/app/ztehelper
-rm -rf $SYSDIR/app/HomeMediaCenter
+sudo rm -f $SYSDIR/app/VideoTestTool.apk
+sudo rm -rf $SYSDIR/app/QuickSearchBox
+sudo rm -rf $SYSDIR/app/NetworkTest
+sudo rm -rf $SYSDIR/app/ztehelper
+sudo rm -rf $SYSDIR/app/HomeMediaCenter
 
-rm -f $SYSDIR/app/IPTV.apk
-rm -f $SYSDIR/app/mcspbase.apk
-rm -f $SYSDIR/app/NaAgent.apk
-rm -f $SYSDIR/app/netmanager.apk
-rm -f $SYSDIR/app/nmAssistant.apk
+sudo rm -f $SYSDIR/app/IPTV.apk
+sudo rm -f $SYSDIR/app/mcspbase.apk
+sudo rm -f $SYSDIR/app/NaAgent.apk
+sudo rm -f $SYSDIR/app/netmanager.apk
+sudo rm -f $SYSDIR/app/nmAssistant.apk
 
 # Cleann!
-rm -f $SYSDIR/app/OSDService.apk
-rm -f $SYSDIR/app/ZeroCfgUI.apk
-rm -f $SYSDIR/app/Dlnagwapt.apk
-rm -f $SYSDIR/app/MSGAPK.apk
+sudo rm -f $SYSDIR/app/OSDService.apk
+sudo rm -f $SYSDIR/app/ZeroCfgUI.apk
+sudo rm -f $SYSDIR/app/Dlnagwapt.apk
+sudo rm -f $SYSDIR/app/MSGAPK.apk
 
-rm -f $SYSDIR/app/dlna.apk
-rm -f $SYSDIR/app/MSGAPKSub.apk
-rm -rf $SYSDIR/app/AuthConfig
-rm -rf $SYSDIR/app/SubtitleService
-rm -rf $SYSDIR/app/FileBrowser
+sudo rm -f $SYSDIR/app/dlna.apk
+sudo rm -f $SYSDIR/app/MSGAPKSub.apk
+sudo rm -rf $SYSDIR/app/AuthConfig
+sudo rm -rf $SYSDIR/app/SubtitleService
+sudo rm -rf $SYSDIR/app/FileBrowser
 
 # MeBox launcher
-rm -f $SYSDIR/app/launcher_tkz4.apk
+sudo rm -f $SYSDIR/app/launcher_tkz4.apk
 
 # Must removed if using GAPPS TV
-rm -f $SYSDIR/app/TVClient.apk
-#rm -rf $SYSDIR/app/ADBSetting
-rm -rf $SYSDIR/app/Camera2
-rm -rf $SYSDIR/app/Music
-rm -rf $SYSDIR/app/DownloadProviderUi
-rm -f $SYSDIR/app/com.google.android.tts-3.10.10-210310101.apk
-rm -rf $SYSDIR/priv-app/Contacts/
-rm -rf $SYSDIR/priv-app/LiveTv/
+sudo rm -f $SYSDIR/app/TVClient.apk
+#sudo rm -rf $SYSDIR/app/ADBSetting
+sudo rm -rf $SYSDIR/app/Camera2
+sudo rm -rf $SYSDIR/app/Music
+sudo rm -rf $SYSDIR/app/DownloadProviderUi
+sudo rm -f $SYSDIR/app/com.google.android.tts-3.10.10-210310101.apk
+sudo rm -rf $SYSDIR/priv-app/Contacts/
+sudo rm -rf $SYSDIR/priv-app/LiveTv/
 
 #Uncomment if you want use hardware keyboard only (no softkeyboard)
 #rm -rf $SYSDIR/app/LatinIME
 #rm -rf $SYSDIR/app/OpenWnn 
 
 echo "Remove Unwanted services"
-rm -f $SYSDIR/bin/netaccess
-rm -f $SYSDIR/bin/depconfig
+sudo rm -f $SYSDIR/bin/netaccess
+sudo rm -f $SYSDIR/bin/depconfig
 
 echo "Bootanimation"
-pushd kitchen/bootanimation
+sudo pushd kitchen/bootanimation
 [ -e ../rootfs/media/bootanimation.zip ] && sudo rm ../rootfs/media/bootanimation.zip
-[ -d ../rootfs/media ] || mkdir ../rootfs/media
+[ -d ../rootfs/media ] || sudo mkdir ../rootfs/media
 sudo zip -0 -r '../rootfs/media/bootanimation.zip' *
 popd
 
 echo "Coying default data"
-[ -d kitchen/rootfs/data_default ] || mkdir -p kitchen/rootfs/data_default/data
+[ -d kitchen/rootfs/data_default ] || sudo mkdir -p kitchen/rootfs/data_default/data
 # Fix data permissions, its changed after checkout from git
-chmod -R og+rw kitchen/rootfs/data_default/data/*
-chmod -R +x kitchen/rootfs/xbin/*
-chmod -R +x kitchen/rootfs/bin/*
+sudo chmod -R og+rw kitchen/rootfs/data_default/data/*
+sudo chmod -R +x kitchen/rootfs/xbin/*
+sudo chmod -R +x kitchen/rootfs/bin/*
 
 echo 'Merge rootfs/* into $SYSDIR/*'
-cp -pruv kitchen/rootfs/* $SYSDIR/
+sudo cp -pruv kitchen/rootfs/* $SYSDIR/
 
 echo "Unmount $SYSDIR"
-umount $SYSDIR
+sudo umount $SYSDIR
 
 if which e2fsck &> /dev/null; then
 	echo "Check/repair file system.img"
-	e2fsck -f $SYSIMG
+	sudo e2fsck -f $SYSIMG
 fi
 
 if which resize2fs &> /dev/null; then
 	# kecilkan partisi biar ga lama bgt ngeflashnya
 	echo "Minimizing system.img"
-	resize2fs -M $SYSIMG
+	sudo resize2fs -M $SYSIMG
 fi
 
 echo "Done, press any button (on the keyboard, not power button) to close."

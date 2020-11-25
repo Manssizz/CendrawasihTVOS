62098068cebce57276f0ecec172307ea398d9ffe
manssizz <fajarmanssizz@gmail.com>
2020-11-25 18:45:19 +0700

    remove u-boot.bin while flashing

--- flash.sh
+++ flash.sh
@@ -3,10 +3,12 @@ UPDATEBIN="./tools/linux/update"
 $UPDATEBIN scan | grep 'No ' && exit 1
 
 echo "FLASHING"
+#[ -e release/u-boot.bin ] && $UPDATEBIN partition bootloader release/u-boot.bin
 [ -e release/boot.img ] && $UPDATEBIN partition boot release/boot.img
 [ -e release/conf.img ] && $UPDATEBIN partition conf release/conf.img
 #[ -e release/logo.img ] && $UPDATEBIN partition logo kitchen/logo.img
 [ -e release/env.img ] && $UPDATEBIN partition env release/env.img
+[ -e release/recovery.img ] && $UPDATEBIN partition recovery release/recovery.img
 $UPDATEBIN partition system mod/system.img
 $UPDATEBIN bulkcmd "amlmmc erase data"
 $UPDATEBIN bulkcmd "amlmmc erase cache"
@@ -15,4 +17,4 @@ $UPDATEBIN bulkcmd "setenv -f EnableSelinux permissive"
 $UPDATEBIN bulkcmd "setenv firstboot 1"
 $UPDATEBIN bulkcmd "saveenv"
 echo "DONE"
-read -n 1
+#read -n 1
